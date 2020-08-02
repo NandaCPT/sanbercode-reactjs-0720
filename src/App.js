a63124181c4eac4c5904d6041d4dd41052a927ef
NandaCPT <nputra71@gmail.com>
2020-08-03 00:57:26 +0700

    git reactjs tugas 10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'container'>
+      <form>
+      <table>
+      <h1 style={{textAlign: "center"}}> Form Pembelian Buah</h1>
+      <tr>
+      <td><b>Nama Pelanggan</b></td>
+      <td> <input type="text" name="pelanggan" /> </td> <br></br>
+      </tr>
+      <tr>
+      <td><b>Daftar Item</b></td>
+      <td><input type="checkbox" name="daftar1" value="semangka" /> Semangka  <br></br>
+      <input type="checkbox" name="daftar2" value="jeruk" /> Jeruk  <br></br>
+      <input type="checkbox" name="daftar3" value="nanas" /> Nanas  <br></br>
+      <input type="checkbox" name="daftar4" value="salak" /> Salak  <br></br>
+      <input type="checkbox" name="daftar5" value="anggur" /> Anggur  <br></br>
+      </td>
+      </tr>
+      </table>
+      </form>
+      
+      <button type="submit">Kirim</button>
     </div>
+      
+  
   );
 }
 
